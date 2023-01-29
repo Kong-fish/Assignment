8f1266f16e696ecdbcd87366b535d450b8741a3a
Kongfish <1221303689@student.mmu.edu.my>
2023-01-29 17:17:54 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,3 @@
+//This will be the main code we will edit and hand in
+//Please familliar with Github cause it shares code and easy to work with more than sharing on googele doc
+// let fight for three days no sleep 
